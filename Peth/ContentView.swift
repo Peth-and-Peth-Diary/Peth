37d06902cef232f70e383d97bcbef552249ec8cb
mohammad subkhan <subkhanpinter22@gmail.com>
2023-08-06 17:21:13 +0700

    Initial Commit

---
+++ Peth/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Peth
+//
+//  Created by masbek mbp-m2 on 06/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Peth/PethApp.swift
@@ -0,0 +1,17 @@
+//
+//  PethApp.swift
+//  Peth
+//
+//  Created by masbek mbp-m2 on 06/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PethApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
